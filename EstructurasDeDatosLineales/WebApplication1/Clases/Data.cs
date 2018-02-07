2180865c2cfa8b6065d724c162b39d47acd0c401
williams <williams@github.com>
2018-02-07 16:15:31 -0600

    Inicio

---
+++ EstructurasDeDatosLineales/WebApplication1/Clases/Data.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebApplication1.Models;
+
+namespace WebApplication1.Clases
+{
+    public class Data
+    {
+        private static Data instance = null;
+
+        public static Data Instance
+        {
+            get {
+                if (instance == null) instance = new Data();
+                return instance;
+            }
+        }
+
+        public List<Jugador> Jugadores = new List<Jugador>();
+    }
+}
---
+++ EstructurasDeDatosLineales/WebApplication1/Controllers/JugadorController.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WebApplication1.Clases;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class JugadorController : Controller
+    {
+        // GET: Jugador
+        public ActionResult Index()
+        {
+            return View(Data.Instance.Jugadores);
+        }
+
+        // GET: Jugador/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Jugador/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Jugador/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                Data.Instance.Jugadores.Add(new Jugador
+                {
+                    id = Data.Instance.Jugadores.Count + 1,
+                    nombre = collection["Nombre"],
+                    apellido = collection["Apellido"]
+
+                });
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logiche re
+                Jugador ed = new Jugador();
+                ed.nombre = collection["Nombre"];
+                ed.apellido = collection["Apellido"];
+                Data.Instance.Jugadores.Insert(id, ed);
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Jugador/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Jugador/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
