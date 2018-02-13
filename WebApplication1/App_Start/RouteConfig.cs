384f5ec65454739a139b402d7bb4d593de987e20
Emmanuel Josué Alvarado Lemus <llaajjee@hotmail.com>
2018-02-13 16:08:14 -0600

    Home page creation /w selection of list-to-use

--- WebApplication1/App_Start/RouteConfig.cs
+++ WebApplication1/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace WebApplication1
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Jugador", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Jugador", action = "Home", id = UrlParameter.Optional }
             );
         }
     }
